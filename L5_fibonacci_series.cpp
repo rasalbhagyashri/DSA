1584d585e7a8a3522a53b2f7a24e0cc80c3aac7e
Bhagyashri Balaji Rasal <bhagyashrirasal711@gmail.com>
2024-09-23 20:48:21 +0530

    L5_fibonacci_series.cpp

--- L5_fibonacci_series.cpp
+++ L5_fibonacci_series.cpp
@@ -4,13 +4,13 @@ int main()
 {
     int n;
     cin >> n;
-    int a=0;
+    int a=0;// input for first two numbers of fibo series 
     int b=1;
      cout << a<< " "<< b <<" ";
 int i=1;
 for (i=1;i<=n;i++)
 {
-    int next_number=a+b;
+    int next_number=a+b;//generation of fibonacci series
     cout << next_number<<" ";
     a= b;
     b= next_number;
